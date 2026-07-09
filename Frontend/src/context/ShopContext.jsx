516b3e62d6922a0cc0f7f8fb04db095d3017b5c5
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2026-07-09 10:09:46 +0530

    palceorder part

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { products } from '../assets/assets';
 import { toast } from 'react-toastify';
 
@@ -10,6 +11,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
         if(!size){
@@ -58,9 +60,23 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
-    useEffect(()=>{
-        // console.log(cartItems);
-    },[cartItems])
+    const getCartAmount = () => {
+        let totalAmount = 0;
+
+        for(const items in cartItems){
+            let itemInfo = products.find((product) => product._id === items);
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item] > 0){
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                }catch(error){
+
+                }
+            }
+        }
+        return totalAmount;
+    }
 
     const value = {
         products,
@@ -73,7 +89,9 @@ const ShopContextProvider = (props) => {
         cartItems,
         addToCart,
         getCartCount,
-        updateQuantity
+        updateQuantity,
+        getCartAmount,
+        navigate
     }
 
     return (

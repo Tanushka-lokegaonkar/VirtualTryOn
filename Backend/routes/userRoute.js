4b1bd760411c1b4bf36b4049574558ee2614b32d
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2026-08-04 12:57:08 +0530

    DB configured

---
+++ Backend/routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { loginUser, registerUser, adminLogin } from '../controllers/userController.js';
+
+const userRouter = express.Router();
+
+// Route for user login
+userRouter.post('/login', loginUser);
+
+// Route for user registration
+userRouter.post('/register', registerUser);
+
+// Route for getting user profile
+userRouter.get('/admin', adminLogin);
+
+export default userRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import e from 'express';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRoutes from './routes/userRoute.js';
 
 // App config
 const app = express();
 const PORT = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
 
 // API Endpoints
+app.use('/api/user', userRoutes);
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
 });

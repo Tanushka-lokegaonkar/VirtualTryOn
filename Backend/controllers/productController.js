5c7344287d5c67304bce728adc2ffb546f7ea455
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2026-08-23 09:53:38 +0530

    login module done

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+
+// function to add a new product
+const addProduct = async (req, res) => {
+
+}
+
+// function for list product
+const listProducts = async (req, res) => {
+
+}
+
+// function to remove a product
+const removeProduct = async (req, res) => {
+
+}
+
+// function for single product info
+const singleProduct = async (req, res) => {
+
+}
+
+export { addProduct, listProducts, removeProduct, singleProduct };
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,13 +1,72 @@
+import userModel from '../models/userModel.js';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: '1d' });
+}
 
 //Route for user login
 const loginUser = async (req, res) => {
+    try{
 
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: 'User does not exist' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            const token = createToken(user._id);
+            return res.status(200).json({ success: true, message: 'User logged in successfully', token });
+        }else {
+            res.json({success: false, message: 'Invalid credentials'});
+        }
+        
+    }catch (error) {
+        res.status(500).json({ success: false, message: 'Error logging in user', error });
+    }
 }
 
 //Route for user registration
 const registerUser = async (req, res) => {
-    res.status(200).json({ message: 'User registered successfully' });
+    try{
+        const { name, email, password } = req.body;
+
+        const exists = await userModel.findOne({ email });
+
+        if (exists) {
+            return res.status(400).json({success:false, message: 'User already exists' });
+        }
+
+        // Validate email
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ success: false, message: 'Invalid email format' });
+        }
+
+        // Validate password
+        if (!validator.isLength(password, { min: 8 })) {
+            return res.status(400).json({ success: false, message: 'Password must be at least 8 characters long' });
+        }
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({ name, email, password: hashedPassword });
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+        res.json({success: true, message: 'User registered successfully', token });
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Error registering user', error });
+    }
 }
 
 //Route for admin login

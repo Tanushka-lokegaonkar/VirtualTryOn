8164961597a16bf246e3969df3f077b3c1bc63ef
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2026-08-24 09:27:09 +0530

    admin panal sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -4,13 +4,24 @@ import { assets } from '../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>
-        <div>
+    <div className='w-[18%] min-h-screen border-r-2'>
+        <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
             
-            <NavLink to="/add">
-                <img src={assets.add_icon} alt="Add"/>
-                <p>Add Items</p>
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-2xl ' to="/add">
+                <img className='w-5 h-5' src={assets.add_icon} alt="Add"/>
+                <p className='hidden md:block' >Add Items</p>
             </NavLink>
+
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-2xl ' to="/list">
+                <img className='w-5 h-5' src={assets.order_icon} alt="Orders"/>
+                <p className='hidden md:block' >List Items</p>
+            </NavLink>
+
+            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-2xl ' to="/orders">
+                <img className='w-5 h-5' src={assets.order_icon} alt="Orders"/>
+                <p className='hidden md:block' >Orders</p>
+            </NavLink>
+
         </div>
     </div>
   )
